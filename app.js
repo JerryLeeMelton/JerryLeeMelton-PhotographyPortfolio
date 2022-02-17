51cf7c50e416d37c45f2e0d0beaa7e9e07bf3e1c
Jerry Lee Melton <jerryleemelton@gmail.com>
2022-02-17 08:40:16 -0800

    Initial commit.  Setup project files.  Add fonts.

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  //res.send("Hello");
+  res.render("home");
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000.");
+});
