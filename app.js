1f7bb2018821a3f5d84a3a6b2cddae7e67b07b28
Jerry Lee Melton <jerryleemelton@gmail.com>
2022-02-19 12:18:13 -0800

    Add base pages for missing pages on the site.  Link up navbar/app to new pages.

--- app.js
+++ app.js
@@ -8,7 +8,31 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/portfolio-abstract", (req, res) => {
   res.render("portfolio-abstract");
+});
+
+app.get("/portfolio-product", (req, res) => {
+  res.render("portfolio-product");
+});
+
+app.get("/portfolio-natureandcity", (req, res) => {
+  res.render("portfolio-natureandcity");
+});
+
+app.get("/portfolio-food", (req, res) => {
+  res.render("portfolio-food");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
 });
 
 app.listen(3000, () => {
