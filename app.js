ce0383ea4871ca7868de4c42c284c4ffe773f64d
Jerry Lee Melton <jerryleemelton@gmail.com>
2022-03-03 15:20:21 -0800

    Add sending functionality to contact form

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const nodemailer = require("nodemailer");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+require("dotenv").config();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -35,6 +38,50 @@ app.get("/about", (req, res) => {
 
 app.get("/contact", (req, res) => {
   res.render("contact");
+});
+
+// Contact Form ===============================================================
+app.post("/", (req, res)=>{
+
+  const transporter = nodemailer.createTransport({
+    host: "server264.web-hosting.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.CONTACT_EMAIL,
+      pass: process.env.CONTACT_PASSWORD
+    }
+  });
+
+  let messageBody = `
+  Message Details:
+
+  Name: ${req.body.name}
+  email: ${req.body.email}
+
+  Message:
+  ${req.body.message}
+  `;
+
+  const mailOptions = {
+    from: process.env.CONTACT_EMAIL,
+    to: process.env.TARGET_EMAIL,
+    subject: "Message from a portfolio site visitor",
+    text: messageBody
+  };
+
+  transporter.sendMail(mailOptions, (error, info)=>{
+    if (error) {
+      console.log(error);
+      res.send("error");
+    }
+    else {
+      console.log("Email sent: " + info.response);
+      res.send("success");
+    }
+
+  });
+
 });
 
 app.listen(PORT, () => {
